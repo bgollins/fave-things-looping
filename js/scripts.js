39e993e1b4477da8483edacd14e4dc688a1acfa3
Brittany Gollins and Michael Butcher <michael@>
2018-09-24 19:59:26 -0700

    take user input, create an array, pull the 2nd, 1st, and 3rd items from the array and spit out for user on html page

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,21 @@ $(document).ready(function() {
     var food = $("#food").val();
     var music = $("#music").val();
     var color = $("#color").val();
-    var results = [name, animal, food, music, color]
+    var results = [name, animal, food, music, color];
+
+    var topThings1 = results[1];
+    var topThings2 = results[0];
+    var topThings3 = results[2];
+    var topTopThings = topThings1.concat(topThings2.concat(topThings3));
+    topTopThings.toString();
+    $("#topTopThings").text(topTopThings);
+    topThings1.toString();
+    $(".topThings1").text(topThings1);
+    topThings2.toString();
+    $(".topThings2").text(topThings2);
+    topThings3.toString();
+    $(".topThings3").text(topThings3);
+
     event.preventDefault();
+  });
 });
