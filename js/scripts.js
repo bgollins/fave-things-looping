35d35bf78dab052693fcc293ac7e8309ce26afb1
Brittany Gollins and Michael Butcher <michael@>
2018-09-24 19:04:45 -0700

    add jquery to take user input and create variables, and put the 5 variables into an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
   $("#favorites form").submit(function(event) {
+    var name = $("#name").val();
+    var animal = $("#animal").val();
+    var food = $("#food").val();
+    var music = $("#music").val();
+    var color = $("#color").val();
+    var results = [name, animal, food, music, color]
     event.preventDefault();
 });
