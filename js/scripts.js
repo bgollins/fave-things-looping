3578b8c177e44b7d1065f83be135497cdc10563e
Brittany Gollins and Michael Butcher <michael@>
2018-09-24 20:48:16 -0700

    use looping to dry up the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,24 @@
 $(document).ready(function() {
   $("#favorites form").submit(function(event) {
-    var name = $("#name").val();
-    var animal = $("#animal").val();
-    var food = $("#food").val();
-    var music = $("#music").val();
-    var color = $("#color").val();
-    var results = [name, animal, food, music, color];
+    var favs = ["name", "animal", "food", "music", "color"];
 
-    var topThings1 = results[1];
-    var topThings2 = results[0];
-    var topThings3 = results[2];
-    var topTopThings = topThings1.concat(topThings2.concat(topThings3));
-    topTopThings.toString();
-    $("#topTopThings").text(topTopThings);
-    topThings1.toString();
-    $(".topThings1").text(topThings1);
-    topThings2.toString();
-    $(".topThings2").text(topThings2);
-    topThings3.toString();
-    $(".topThings3").text(topThings3);
-
+    favs.forEach(function(fav) {
+     var userInput = $("#" + fav).val();
+     $("." + fav).text(userInput);
+    });
     event.preventDefault();
   });
 });
+
+    // var topThings1 = results[1];
+    // var topThings2 = results[0];
+    // var topThings3 = results[2];
+    // var topTopThings = topThings1.concat(topThings2.concat(topThings3));
+    // topTopThings.toString();
+    // $("#topTopThings").text(topTopThings);
+    // topThings1.toString();
+    // $(".topThings1").text(topThings1);
+    // topThings2.toString();
+    // $(".topThings2").text(topThings2);
+    // topThings3.toString();
+    // $(".topThings3").text(topThings3);
